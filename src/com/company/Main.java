2e7213575a9e1dc4aae42d793a2da4478e4a57fc
Rubén Triviño Juárez <ruben@willimbu.com>
2018-03-01 19:46:15 +0100

    Añade mi local path

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String inputFile ="/Users/pablo/IdeaProjects/HashCode2018/src/com/company/a_example.in";
-        String outputFile ="/Users/pablo/IdeaProjects/HashCode2018/src/com/company/a_example.out";
+        // String inputFile ="/Users/pablo/IdeaProjects/HashCode2018/src/com/company/a_example.in";
+        // String outputFile ="/Users/pablo/IdeaProjects/HashCode2018/src/com/company/a_example.out";
+
+        String inputFile ="/Users/benru/Workspace/hashCode2018/src/com/company/a_example.in";
+        String outputFile ="/Users/benru/Workspace/hashCode2018/src/com/company/a_example.out";
         IOFileManager fileManager = new IOFileManager(inputFile, outputFile);
 
 

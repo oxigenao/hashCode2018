5b69a2688c881f1b89f5e33b3b6fae47d6821969
Pablo Ortuño <pablo@willimbu.com>
2018-03-01 19:45:25 +0100

    data class in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,20 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<ArrayList<String>>  data;
 
         String inputFile ="/Users/pablo/IdeaProjects/HashCode2018/src/com/company/a_example.in";
         String outputFile ="/Users/pablo/IdeaProjects/HashCode2018/src/com/company/a_example.out";
         IOFileManager fileManager = new IOFileManager(inputFile, outputFile);
 
 
-        System.out.println(fileManager.ReadFromFile());
+        data = fileManager.ReadFromFile();
+        System.out.println(data);
 
 
     }
